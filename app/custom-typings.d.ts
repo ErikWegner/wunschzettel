29867d208a56a9d0e6ecd0b171fab8fc75a04607
Erik Wegner <E_Wegner@web.de>
2016-06-23 20:54:13 +0200

    Aufräumen

--- app/custom-typings.d.ts
+++
@@ -1,8 +0,0 @@
-interface IDialogInterface {
-  showModal(): void
-  close(): void
-}
-interface IDialogPolyfill {
-  registerDialog(dialog: IDialogInterface): void
-}
-declare var dialogPolyfill: IDialogPolyfill
--- app/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-
-// Add all operators to Observable
-//import 'rxjs/Rx';
-import './add-rxjs-operators';
-
-import { AppComponent } from './app.component';
-
-bootstrap(AppComponent);
